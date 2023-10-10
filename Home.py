00be88cce1be0b83cebe1f8db26656c51d02a734
Athitaya123 <athitayakakandee@gmail.com>
2023-10-10 12:04:07 +0700

    .commit

--- Home.py
+++ Home.py
@@ -13,12 +13,10 @@ def load_lottieurl(url: str):
 st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
 st.header("Athitaya Kakandee")
 
-lot3="https://lottie.host/57654ef5-32e3-448d-b5f6-ed845b245c93/nsc1xfV677.json"
-lottie3 = load_lottieurl(lot3)
-#st_lottie(lottie3)
-
 col1, col2 = st.columns(2)
 with col1:
+    lot3="https://lottie.host/57654ef5-32e3-448d-b5f6-ed845b245c93/nsc1xfV677.json"
+    lottie3 = load_lottieurl(lot3)
     st_lottie(lottie3)
 with col2:
     st.image('./image/athitaya.jpg')
