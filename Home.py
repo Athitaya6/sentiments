b1a3331a5982c29c775a63abff8326f0f1a621c8
Athitaya123 <athitayakakandee@gmail.com>
2023-10-10 11:15:04 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,6 @@
 import json
 import time
 import requests
-
 import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
@@ -12,15 +11,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/cc59e511-0579-406f-99d5-78170784212f/t6xTGIMm6t.json"
-lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-lottie_download = load_lottieurl(lottie_url_download)
-
-
-st_lottie(lottie_hello, key="hello")
-
-if st.button("Download"):
-    with st_lottie_spinner(lottie_download, key="download"):
-        time.sleep(5)
-    st.balloons()
+lot3="https://lottie.host/2e099151-4397-40ad-ae7b-249bbaf1e8df/gluEhZplO8.json"
+lottie3 = load_lottieurl(lot3)
+st_lottie(lottie3)
+st.balloons()
