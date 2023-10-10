7ddae333f62fac9614e9d231e5879c3037464cf3
Athitaya123 <athitayakakandee@gmail.com>
2023-10-10 11:07:07 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,26 @@
-import streamlit as st
-import pandas as pd
+import json
+import time
+import requests
 
-st.header("😫😁🥶 โปรเจคการวิเคราะห์ความรู้สึกภาษาไทย 🤗🤐😍")
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://lottie.host/cc59e511-0579-406f-99d5-78170784212f/t6xTGIMm6t.json"
+lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+lottie_download = load_lottieurl(lottie_url_download)
+
+
+st_lottie(lottie_hello, key="hello")
+
+if st.button("Download"):
+    with st_lottie_spinner(lottie_download, key="download"):
+        time.sleep(5)
+    st.balloons()
